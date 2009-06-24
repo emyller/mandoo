500d3befd6e45c9bc231a1744e351c28096ab86f
Evandro Myller <emyller@emyller-laptop.(none)>
2009-06-24 04:21:31 -0300

    basic shadow effect implemented

--- mod/ui_basics.js
+++ mod/ui_basics.js
@@ -17,7 +17,8 @@ u.mod(
 /* core */ {
 	zIndex: 100,
 
-	modal: function (enable) {
+	modal: function (enable)
+	{
 		var modal = u('#utm_modal');
 
 		enable = enable === undefined ?
@@ -25,7 +26,8 @@ u.mod(
 			!!enable;
 
 		// creates the modal
-		if (enable && !modal[0]) {
+		if (enable && !modal[0])
+		{
 			modal = u.append('div#utm_modal')
 				.css('opacity', .5)
 				.front();
@@ -38,7 +40,8 @@ u.mod(
 		}
 
 		// removes the modal
-		else if (!enable) {
+		else if (!enable)
+		{
 			modal.remove();
 			clearInterval(u.modal.refresh);
 		}
@@ -46,32 +49,65 @@ u.mod(
 },
 
 /* elements methods */ {
-	front: function () {
+	front: function ()
+	{
 		for (var i = -1; this[++i];)
 			this[i].style.zIndex = u.zIndex++;
 
 		return this;
 	},
 
-	selectable: function (enable) {
-	//>> [de]activates content selection on elements
+	selectable: function (enable)
+	{//>> [de]activates content selection on elements
 	// note: in Opera, the [event].preventDefault method must be called.
 		this
 		.css('-moz-user-select', enable? '' : 'none')
 		.attr('unselectable', enable? 'off' : 'on');
 
 		enable ?
-			this.listen('selectstart', __preventSelect) :
-			this.unlisten('selectstart', __preventSelect);
+			this.listen('selectstart', preventSelect) :
+			this.unlisten('selectstart', preventSelect);
 
 		return this;
+	},
+
+	shadow: function (options)
+	{
+		options = u.extend(
+		{
+			distance: 5,
+			opacity: .3
+		},
+		options || {});
+
+		for (var i = -1; this[++i];)
+		if (!this[i].shadow)
+		{
+			var pos = u(this[i]).pos(),
+				size = u.size(this);
+
+			this[i].shadow = u.append('div.utm_shadow').css(
+			{
+				width: size.width,
+				height: size.height,
+				left: pos.left + options.distance,
+				top: pos.top + options.distance,
+				zIndex: z = +u(this[i]).css('z-index') || 0,
+				opacity: options.opacity
+			});
+		}
+		else
+		{
+			u(this[i].shadow).remove(true);
+			delete this[i].shadow;
+		}
 	}
 }
 
 );
 
 // simple standard function to prevent selection
-function __preventSelect(e) {
+function preventSelect(e) {
 	e.preventDefault();
 }
 
