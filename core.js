9fad7a9be8597757d59815516ba12b61962555e4
Evandro Myller <evandromyller@gmail.com>
2010-05-28 14:35:27 -0300

    tagging 2.1 beta1

--- core.js
+++ core.js
@@ -8,7 +8,7 @@ Mandoo = function (s, c) {
 	return new u.__init__(s, c); };
 
 (function (u) {
-u.__version__ = 2.03;
+u.__version__ = 2.09;
 
 u.__init__ = function (sel, context) {
 	if (sel && sel.__mandoo__)
